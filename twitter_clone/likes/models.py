08f499311daa8bd9d2874d7601d7f98381c0cbbc
andreiraphel <andreiraphel@gmail.com>
2024-07-17 10:06:53 +0800

    details.html update

--- twitter_clone/likes/models.py
+++ twitter_clone/likes/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from users.models import User
 from tweets.models import Tweet
 
-class Likes(models.Model):
+class Like(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    tweet_liked = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
     date_liked = models.DateField(auto_now_add=True)
--- twitter_clone/users/views.py
+++ twitter_clone/users/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import User
 from tweets.models import Tweet
+from likes.models import Like
 
 # Create your views here.
 def users(request):
@@ -18,17 +19,27 @@ def users(request):
 def details(request, id):
     myuser = User.objects.get(id=id)
     tweets = Tweet.objects.all()
+    likes = Like.objects.all()
 
     user_tweets = []
+    tweet_likes = {}
+
     for tweet in tweets:
         if tweet.user_id == id:
             user_tweets.append(tweet)
+            tweet_likes[tweet.id] = 0
+            for like in likes:
+                if like.tweet_id == tweet.id:
+                    tweet_likes[tweet.id] += 1
+                    tweet_id = tweet.id
 
     template = loader.get_template('details.html')
-
+    print(tweet_likes)
     context = {
         'myuser': myuser,
-        'user_tweets': user_tweets
+        'user_tweets': user_tweets,
+        'tweet_likes': tweet_likes,
+        'tweet_id': tweet_id,
     }
 
     return HttpResponse(template.render(context, request))
