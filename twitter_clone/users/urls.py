41450b49227f1c7b368913ffed946accc85a29bd
andreiraphel <andreiraphel@gmail.com>
2024-07-18 10:29:40 +0800

    liked html

--- twitter_clone/users/urls.py
+++ twitter_clone/users/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('users/', views.users, name='users'),
-    path('users/details/<int:id>', views.details, name='details'),
+    path('users/details/<int:user_id>', views.details, name='details'),
+    path('users/details/<int:user_id>/<int:object_id>', views.liked, name='liked'),
 ]
